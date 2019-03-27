f7b9fa1be9b80fba50f82c1eb843f118d49047b2
Ken Ruf <ruf.kenichi@gmail.com>
2019-03-27 02:54:09 -0500

    finished arrow function conversions

--- assignments/objects.js
+++ assignments/objects.js
@@ -89,7 +89,18 @@ console.log(Antonietta["gender"]);
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {}
+const parent = {
+  "name": "Susan",
+  "age": 70
+  const child = {
+    "name": "George",
+    "age": 50
+    const grandchild = {
+      "name": "Sam",
+      "age":30
+    }
+  }
+}
 
 // Log the parent object's name
 
